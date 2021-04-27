75ae8738a8a1a0aa937fc573f5fda4125c8ee2ef
Pedro Lages <pedro.lages@hotmart.com>
2021-04-27 11:47:38 -0300

    implement multithread build

--- lib/reports_generator_challenge.ex
+++ lib/reports_generator_challenge.ex
@@ -16,9 +16,43 @@ defmodule ReportsGeneratorChallenge do
     12 => "dezembro",
   }
 
-  def build do
-    Parser.parse_file("report.csv")
+  def build(filename) do
+    Parser.parse_file(filename)
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def build_from_many(filenames) when not is_list(filenames), do: {:error, "Please provide a list of strings"}
+
+  def build_from_many(filenames) do
+    result = filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
+  defp sum_reports(%{
+      "all_hours" => all_hours1,
+      "hours_per_month" => hours_per_month1,
+      "hours_per_year" => hours_per_year1,
+    }, %{
+      "all_hours" => all_hours2,
+      "hours_per_month" => hours_per_month2,
+      "hours_per_year" => hours_per_year2,
+    }) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+    hours_per_month = merge_maps_maps(hours_per_month1, hours_per_month2)
+    hours_per_year = merge_maps_maps(hours_per_year1, hours_per_year2)
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
+  defp merge_maps_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> merge_maps(value1, value2) end)
   end
 
   defp sum_values([name, work_hours, _day, month, year], %{
